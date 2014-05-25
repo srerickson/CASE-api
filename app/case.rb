0fdf8aa2bd960600c68dc08f9eded7f48fd50dfd
Seth Erickson <sr.erickson@gmail.com>
2014-05-25 13:20:08 -0700

    general case fields

--- app/case.rb
+++ app/case.rb
@@ -1,9 +1,13 @@
 class Case
   include Mongoid::Document
   include Mongoid::Timestamps
+
   field :name, type: String 
   validates :name, presence: true
+
   field :description, type: String 
+  field :summary, type: String
+  field :credits, type: String
 
   has_many :field_values 
 
