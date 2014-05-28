871f186179b87366422ae7ca69d97d8ea499fbea
Seth Erickson <sr.erickson@gmail.com>
2014-05-27 23:52:08 -0700

    some json/test data

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-curl -X PUT \
+curl -X POST \
      -H 'Content-Type:application/json' \
-     -d '{"field_definition": {"name": "URL"}}' \
-     http://localhost:3000/field_sets/538544013d3d8f018b000001/field_definitions/538544013d3d8f018b000002
+     -d '{"username": "sr.erickson@gmail.com", "password": ""}' \
+     http://localhost:3000/authenticate
+
+curl -X GET \
+     -H 'Content-Type:application/json' \
+     -H 'Authorization: eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJfaWQiOiI1Mzg1Nzk4NDNkM2Q4ZjAyYTMwMDAwMDIiLCJuYW1lIjoiU2V0aCIsImVtYWlsIjoic3IuZXJpY2tzb25AZ21haWwuY29tIiwidXBkYXRlZF9hdCI6IjIwMTQtMDUtMjhUMDU6NTI6MDRaIn0.suu8AmPwtdn4RA8yQ86y4X-rqsGvPPPEYv5brJr0nso' \
+     http://localhost:3000/field_sets
