a6ed86cecbc31342fe7ec7ee0a3e79fc622d150f
Seth Erickson <sr.erickson@gmail.com>
2014-05-29 18:48:06 -0700

    some notes on querying field values by their json data

--- app/field_value.rb
+++ app/field_value.rb
@@ -19,4 +19,11 @@ class FieldValue < ActiveRecord::Base
   #   @field_definition ||= parent_field_set.nil? ? nil : parent_field_set.field_definitions.detect{|fd| fd._id == field_definition_id}
   # end
 
+  # Example of searching by json value
+  # FieldValue.where("value->>'text' = 'onetwothree'")
+
+
+  # FieldValue.where("(select json_object_keys(value)) = 'array'").size
+  # select * from field_values where (select json_object_keys(value)) = 'array';
+
 end
