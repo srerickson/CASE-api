5664e76f2ceb953ed735a6f85971b3f9b84c5028
Seth Erickson <sr.erickson@gmail.com>
2014-06-17 14:30:57 -0700

    more BOI field set definitions

--- field_sets/create.sh
+++ field_sets/create.sh
@@ -14,17 +14,42 @@ echo "... authorized?: $AUTH"
 
 
 
-FS_ID=`curl -s -X POST \
+BI_ID=`curl -s -X POST \
      -H 'Content-Type:application/json' \
      -d @basic_info.json \
      -H "Authorization: $TOKEN" \
-     http://localhost:3000/field_sets | jsawk "return this.id" ` 
-echo "... created Basic Info field set with id: $FS_ID"
+     http://localhost:3000/field_sets | jsawk "return this.field_set.id" ` 
+echo "... created Basic Info field set with id: $BI_ID"
 
 
-FS_ID=`curl -s -X POST \
+ST_ID=`curl -s -X POST \
      -H 'Content-Type:application/json' \
      -d @structure.json \
      -H "Authorization: $TOKEN" \
-     http://localhost:3000/field_sets | jsawk "return this.id" ` 
-echo "... created structure field set with id: $FS_ID"
+     http://localhost:3000/field_sets | jsawk "return this.field_set.id" ` 
+echo "... created structure field set with id: $ST_ID"
+
+
+ON_ID=`curl -s -X POST \
+     -H 'Content-Type:application/json' \
+     -d @ontogeny.json \
+     -H "Authorization: $TOKEN" \
+     http://localhost:3000/field_sets | jsawk "return this.field_set.id" ` 
+echo "... created structure field set with id: $ON_ID"
+
+
+BE_ID=`curl -s -X POST \
+     -H 'Content-Type:application/json' \
+     -d @behavior.json \
+     -H "Authorization: $TOKEN" \
+     http://localhost:3000/field_sets | jsawk "return this.field_set.id" ` 
+echo "... created structure field set with id: $BE_ID"
+
+
+SCHEMA_ID=`curl -s -X POST \
+     -H 'Content-Type:application/json' \
+     -d "{\"schema\": {\"name\": \"Birds of the Internet\", \"param\":\"boi\", \"field_set_ids\": [$BI_ID, $ST_ID, $ON_ID, $BE_ID]}}"\
+     -H "Authorization: $TOKEN" \
+     http://localhost:3000/schemas | jsawk "return this.schema.id" ` 
+echo "... created BOI schema with id: $SCHEMA_ID"
+
