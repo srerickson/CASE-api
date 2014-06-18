5afb0b1ad84860996fab6a3fec57d27c6af86dd2
Seth Erickson <sr.erickson@gmail.com>
2014-06-18 10:07:18 -0700

    field definitions for BOI mating field set

--- field_sets/create.sh
+++ field_sets/create.sh
@@ -19,7 +19,7 @@ BI_ID=`curl -s -X POST \
      -d @basic_info.json \
      -H "Authorization: $TOKEN" \
      http://localhost:3000/field_sets | jsawk "return this.field_set.id" ` 
-echo "... created Basic Info field set with id: $BI_ID"
+echo "... created basic info field set with id: $BI_ID"
 
 
 ST_ID=`curl -s -X POST \
@@ -35,7 +35,7 @@ ON_ID=`curl -s -X POST \
      -d @ontogeny.json \
      -H "Authorization: $TOKEN" \
      http://localhost:3000/field_sets | jsawk "return this.field_set.id" ` 
-echo "... created structure field set with id: $ON_ID"
+echo "... created ontogeny field set with id: $ON_ID"
 
 
 BE_ID=`curl -s -X POST \
@@ -43,12 +43,19 @@ BE_ID=`curl -s -X POST \
      -d @behavior.json \
      -H "Authorization: $TOKEN" \
      http://localhost:3000/field_sets | jsawk "return this.field_set.id" ` 
-echo "... created structure field set with id: $BE_ID"
+echo "... created behavior field set with id: $BE_ID"
+
+MA_ID=`curl -s -X POST \
+     -H 'Content-Type:application/json' \
+     -d @mating.json \
+     -H "Authorization: $TOKEN" \
+     http://localhost:3000/field_sets | jsawk "return this.field_set.id" ` 
+echo "... created mating field set with id: $MA_ID"
 
 
 SCHEMA_ID=`curl -s -X POST \
      -H 'Content-Type:application/json' \
-     -d "{\"schema\": {\"name\": \"Birds of the Internet\", \"param\":\"boi\", \"field_set_ids\": [$BI_ID, $ST_ID, $ON_ID, $BE_ID]}}"\
+     -d "{\"schema\": {\"name\": \"Birds of the Internet\", \"param\":\"boi\", \"field_set_ids\": [$BI_ID, $ST_ID, $ON_ID, $BE_ID, $MA_ID]}}"\
      -H "Authorization: $TOKEN" \
      http://localhost:3000/schemas | jsawk "return this.schema.id" ` 
 echo "... created BOI schema with id: $SCHEMA_ID"
