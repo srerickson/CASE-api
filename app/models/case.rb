01b923f1f72a8ae643b3329f5ba1962059b347af
Seth Erickson <sr.erickson@gmail.com>
2014-06-21 10:00:31 -0700

    struggling to access uniq cases from schema -- see: https://github.com/rails/rails/issues/13677

--- app/models/case.rb
+++ app/models/case.rb
@@ -4,7 +4,7 @@ class Case < ActiveRecord::Base
 
   has_many :field_values, dependent: :destroy, inverse_of: :case
   has_many :field_definitions, through: :field_values
-  has_many :field_sets, through: :field_definitions
-  has_many :schemas, through: :field_definitions
+  has_many :field_sets,  -> { uniq }, through: :field_definitions
+  has_many :schemas, -> { uniq },through: :field_sets
 
 end
--- app/models/schema.rb
+++ app/models/schema.rb
@@ -5,10 +5,24 @@ class Schema  < ActiveRecord::Base
                         dependent: :destroy, 
                         inverse_of: :schema
 
+  has_many :field_definitions, through: :field_sets
+
+  ## These don't work because of https://github.com/rails/rails/issues/13677
+  ## (can't do uniq) 
+  #
+  # has_many :field_values, through: :field_definitions
+  # has_many :cases, through: :field_values
+
   belongs_to :user 
+
   validates_presence_of :name, :param
 
   accepts_nested_attributes_for :field_sets, allow_destroy: true
+
+
+  def cases 
+    Case.uniq.joins(:schemas).merge(Schema.where(id: self.id ))
+  end
 
 
 end
