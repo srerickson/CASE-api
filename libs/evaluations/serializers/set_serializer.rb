bb64b92444aba8e91fd4afabee83143ad630fb06
Seth Erickson <sr.erickson@gmail.com>
2014-07-16 15:43:53 -0700

    get rid of aggregates field in default eval set json

--- libs/evaluations/serializers/set_serializer.rb
+++ libs/evaluations/serializers/set_serializer.rb
@@ -2,14 +2,14 @@ module CASE
   module Evaluations
     class SetSerializer < CompactSetSerializer
 
-      attributes :aggregates
+      # attributes :aggregates
 
-      def aggregates
-        # only show aggregate if Set is public or user is set's owner 
-        if object.public_responses or (scope and object.user_id == scope.id)
-          object.aggregates
-        end
-      end
+      # def aggregates
+      #   # only show aggregate if Set is public or user is set's owner 
+      #   if object.public_responses or (scope and object.user_id == scope.id)
+      #     object.aggregates
+      #   end
+      # end
 
     end
   end
