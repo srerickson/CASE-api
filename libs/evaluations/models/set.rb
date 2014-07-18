18b317c9642050c5c4d785e6e780da43d8cd2637
Seth Erickson <sr.erickson@gmail.com>
2014-07-18 15:10:03 -0700

    order evaluation questions relationship

--- libs/evaluations/models/set.rb
+++ libs/evaluations/models/set.rb
@@ -2,9 +2,10 @@ module CASE
   module Evaluations
     class Set < ActiveRecord::Base
       belongs_to :user
-      has_many :questions, dependent: :destroy, 
-                           inverse_of: :set,
-                           class_name: '::CASE::Evaluations::Question'
+      has_many :questions,  -> {order(:order)},
+                            dependent: :destroy, 
+                            inverse_of: :set,
+                            class_name: '::CASE::Evaluations::Question'
 
       has_many :responses, through: :questions,
                            class_name: '::CASE::Evaluations::Response'
