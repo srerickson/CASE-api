69dceb5ac1f1c855edf562fb3320bd8103e63fc8
Seth Erickson <sr.erickson@gmail.com>
2014-07-18 14:26:18 -0700

    validate presence of user in eval responess

--- libs/evaluations/models/response.rb
+++ libs/evaluations/models/response.rb
@@ -5,7 +5,7 @@ module CASE
       belongs_to :case #kase?
       belongs_to :question, class_name: '::CASE::Evaluations::Question'
       has_one :set, through: :question, class_name: '::CASE::Evaluations::Set'
-      validates_presence_of :case, :question #, :user
+      validates_presence_of :case, :question, :user
     end
   end
 end
