bd38688a405489b27349cfb9b2ac3102fdc41766
Seth Erickson <sr.erickson@gmail.com>
2014-07-19 20:10:12 -0700

    tweaks to upload json

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -4,4 +4,10 @@ class Upload < ActiveRecord::Base
 
   belongs_to :parent, polymorphic: true, inverse_of: :uploads
 
+  belongs_to :uploaded_by, class_name: "::User"
+
+  def image_and_not_svg?
+    content_type and content_type.start_with? 'image' and content_type !~ /\/svg\+xml/
+  end
+
 end
--- app/serializers/upload_serializer.rb
+++ app/serializers/upload_serializer.rb
@@ -1,20 +1,35 @@
 class UploadSerializer < ActiveModel::Serializer
 
   attributes  :id,
+              :filename,
               :_urls, 
               :content_type,
               :file_size,
               :width, 
               :height, 
               :caption
+              :created_at
+              
+  has_one :uploaded_by
 
   # has_one :uploaded_by, serializer: UserSerializer
 
+  def filename
+    File.basename(object.asset.path)
+  end
+
   def _urls
-    {
-      original: object.asset.url,
-      sq60: object.asset.url(:sq60)
-    }
+    ret = {}
+    ret[:original] = object.asset.url
+
+    if object.image_and_not_svg? 
+      ret[:sq60] = object.asset.url(:sq60)
+      ret[:sq120] = object.asset.url(:sq120)
+
+    end
+
+    ret
+
   end
 
 
